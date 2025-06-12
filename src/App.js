a083dc08303b3e31310efad2f56bd708f8a450e0
egebartukurtaran <egebartu@gmail.com>
2025-06-12 17:22:07 +0300

    css updates and polishing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
     return (
         <div className="min-h-screen bg-gradient-to-r from-indigo-500 to-blue-500 px-4 py-4 sm:py-8">
-            <div className="container bg-white rounded-xl mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl">
+            <div className="container bg-white rounded-xl mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl p-4 sm:p-6 lg:p-8">
                 <Header />
                 <div className="bg-white rounded-xl shadow-lg border border-gray-100 p-4 sm:p-6 lg:p-8">
                     <TodoInput />
